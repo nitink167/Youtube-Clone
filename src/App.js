5be048c4269c5c547d7ced1f893d3eebc639a1df
Nitin Kumar <cool16.nitin@gmail.com>
2020-02-04 18:05:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+//MUI
+import { Grid } from '@material-ui/core';
+
+//Youtube API
+import youtube from './api/youtube'
+
+//components
+import VideoDetail from './components/VideoDetail'
+import SearchBar from './components/SearchBar'
+import VideoList from './components/VideoList'
+
+class App extends React.Component {
+	constructor() {
+		super()
+		this.state = {
+			videos: [],
+			selectedVideo: null
+		}
+	}
+
+	componentDidMount(){
+		this.handleSubmit('React library is best')
+	}
+
+	handleSubmit = async (searchTerm) => {
+		const response = await youtube.get('search', {
+			params: {
+				part: 'snippet',
+				maxResults: 5,
+				key: 'AIzaSyAv5j3RJuxwnyIM1BroX2QBtAaj8HwMQOc',
+				q: searchTerm
+			}
+		})
+
+		this.setState({
+			videos: response.data.items,
+			selectedVideo: response.data.items[0]
+		})
+	}
+
+	onVideoSelect = (video) => {
+		this.setState({ selectedVideo: video})
+	}
+
+	render() {
+		const { selectedVideo, videos } = this.state;
+
+		return (
+			<Grid container spacing={5}>
+				<Grid item xs={12}>
+					<Grid container spacing={2}>
+						<Grid item xs={12}>
+							<SearchBar onFormSubmit={this.handleSubmit}/>
+						</Grid>
+						<Grid item xs={8}>
+							<VideoDetail video={selectedVideo}/>
+						</Grid>
+						<Grid item xs={4}>
+							<VideoList videos={videos} onVideoSelect={this.onVideoSelect}/>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
+		)
+	}
 }
 
-export default App;
+export default App
---
+++ src/components/VideoItem.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {Grid, Paper, Typography} from '@material-ui/core'
+
+const VideoItem = ({ video, onVideoSelect }) => {
+	return(
+		<Grid item xs={12}>
+			<Paper style={{ alignItems: 'center', cursor: 'pointer'}} onClick={() => onVideoSelect(video)}>
+				<img alt='thumbnail' src={video.snippet.thumbnails.medium.url} />
+				<Typography variant='subtitle2'><b>{video.snippet.title}</b></Typography>
+			</Paper>
+		</Grid>
+	)
+}
+
+export default VideoItem;
